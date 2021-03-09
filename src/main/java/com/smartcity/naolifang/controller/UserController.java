f91ed34307e95138671cb39c93a9c969e699ee03
acmren <414443644@qq.com>
2021-03-09 14:38:52 +0800

    【修复】修复账号错误提示的文案问题

--- src/main/java/com/smartcity/naolifang/controller/UserController.java
+++ src/main/java/com/smartcity/naolifang/controller/UserController.java
@@ -80,6 +80,9 @@ public class UserController {
 
         // 1. 判断用户是否合法
         User user = userService.getOne(new QueryWrapper<User>().eq("username", username));
+        if (null == user) {
+            return Result.fail(500, "登录失败，信息：输入的账号或密码错误");
+        }
         String salt = user.getSalt();
         String temp = MD5Util.passwordMd5Encode(password, salt);
 //        String validateCode = request.getSession().getAttribute("validateCode").toString();
